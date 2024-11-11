1db036f3087fa5d4d645780f02fa78839b79ee67
DiiceThief <solarhog87@gmail.com>
2024-11-10 19:34:22 -0700

    Initial commit

---
+++ src/main/java/net/diice/gloomwoodmod/GloomwoodMod.java
@@ -0,0 +1,15 @@
+package net.diice.gloomwoodmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class GloomwoodMod implements ModInitializer {
+	public static final String MOD_ID = "gloomwood-mod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/diice/gloomwoodmod/GloomwoodModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.diice.gloomwoodmod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class GloomwoodModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
