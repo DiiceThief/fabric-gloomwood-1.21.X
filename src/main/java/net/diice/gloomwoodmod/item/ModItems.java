7be9402053869be05c83530ef73fb336412816a5
DiiceThief <solarhog87@gmail.com>
2024-11-22 18:46:46 -0700

    i'm figuring it out

--- src/main/java/net/diice/gloomwoodmod/item/ModItems.java
+++ src/main/java/net/diice/gloomwoodmod/item/ModItems.java
@@ -19,16 +19,12 @@ public class ModItems {
 
     public static final Item GLOOM_RESIN_SWORD = registerItem("gloom_resin_sword", new SwordItem(ModToolMaterials.GLOOM_RESIN,
             new Item.Settings().attributeModifiers(SwordItem.createAttributeModifiers(ModToolMaterials.GLOOM_RESIN, 3, -2.4f))));
-
     public static final Item GLOOM_RESIN_PICKAXE = registerItem("gloom_resin_pickaxe", new PickaxeItem(ModToolMaterials.GLOOM_RESIN,
             new Item.Settings().attributeModifiers(PickaxeItem.createAttributeModifiers(ModToolMaterials.GLOOM_RESIN, 3, -2.4f))));
-
     public static final Item GLOOM_RESIN_AXE = registerItem("gloom_resin_axe", new AxeItem(ModToolMaterials.GLOOM_RESIN,
             new Item.Settings().attributeModifiers(AxeItem.createAttributeModifiers(ModToolMaterials.GLOOM_RESIN, 6, -3.0f))));
-
     public static final Item GLOOM_RESIN_SHOVEL = registerItem("gloom_resin_shovel", new ShovelItem(ModToolMaterials.GLOOM_RESIN,
             new Item.Settings().attributeModifiers(ShovelItem.createAttributeModifiers(ModToolMaterials.GLOOM_RESIN, 1.5f, -3.0f))));
-
     public static final Item GLOOM_RESIN_HOE = registerItem("gloom_resin_hoe", new HoeItem(ModToolMaterials.GLOOM_RESIN,
             new Item.Settings().attributeModifiers(HoeItem.createAttributeModifiers(ModToolMaterials.GLOOM_RESIN, 0, -3.0f))));
 
