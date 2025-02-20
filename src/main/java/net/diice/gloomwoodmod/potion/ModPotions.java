282f1ca007c28e6d97b44974e854395349b97ded
DiiceThief <solarhog87@gmail.com>
2025-02-19 18:54:23 -0700

    first potion effect

--- src/main/java/net/diice/gloomwoodmod/potion/ModPotions.java
+++ src/main/java/net/diice/gloomwoodmod/potion/ModPotions.java
@@ -11,7 +11,7 @@ import net.minecraft.registry.entry.RegistryEntry;
 import net.minecraft.util.Identifier;
 
 public class ModPotions {
-    public static final RegistryEntry<Potion> DETONATION_POTION = registerPotion("detonation potion",
+    public static final RegistryEntry<Potion> DETONATION_POTION = registerPotion("detonation_potion",
             new Potion(new StatusEffectInstance(ModEffects.DETONATION, 1200, 0)));
     private static RegistryEntry<Potion> registerPotion(String name, Potion potion) {
         return Registry.registerReference(Registries.POTION, Identifier.of(GloomwoodMod.MOD_ID, name), potion);
